2294d36924f1f4341fcb7180f7491a728f6c912d
luccaki <lucca.ianaguivara@gmail.com>
2023-05-05 18:00:52 -0300

    finished post_service

--- python_service/src/api/master.py
+++ python_service/src/api/master.py
@@ -3,12 +3,8 @@ from services.file_services import *
 
 master_route = Blueprint('master_route', __name__)
 
-@master_route.route("/api/v1/teste", methods=['GET'])
-def teste():
-    return "teste"
-
 @master_route.route("/api/v1/<provider>/file", methods=['POST'])
 def post_file(provider):
     file = request.files['file']
     res = post_service(provider, file)
-    return Response(res, status=res.status_code )
+    return res
--- python_service/src/services/file_services.py
+++ python_service/src/services/file_services.py
@@ -1,5 +1,6 @@
 import requests
-from services.credentials_service import *
+from flask import Response
+from utils.credentials import get_s3_credentials, get_google_credentials
 
 def post_service(provider, file):
     if(provider == 's3'):
@@ -9,6 +10,23 @@ def post_service(provider, file):
             'Access-Key': credentials['access_key'],
             'Secret-Key': credentials['secret_key']
         }
-        files = {'file': (file.filename, file)}
-        response = requests.post(url, headers=headers, files=files)
-        return response
+    elif(provider == 'googledrive'):
+        credentials = get_google_credentials()
+        url = 'http://app_google_drive:3003/googledrive/v1/file'
+        headers = {
+            'Project-Id': credentials['project_id'],
+            'Private-Key-Id': credentials['private_key_id'],
+            'Private-Key': credentials['private_key'],
+            'Client-Email': credentials['client_email'],
+            'Client-Id': credentials['client_id'],
+            'Client-X509-Cert-Url': credentials['client_x509_cert_url']
+        }
+    elif(provider == 'ipfs'):
+        url = 'http://app_ipfs:3004/ipfs/v1/file'
+        headers = {}
+    else:
+        return f'Wrong provider specified! ({provider})', 400
+    
+    files = {'file': (file.filename, file)}
+    response = requests.post(url, headers=headers, files=files)
+    return Response(response, response.status_code)
