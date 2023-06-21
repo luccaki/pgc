29f8e6529b76dcf25af3e8d4d55bddc26633dacf
luccaki <lucca.ianaguivara@gmail.com>
2023-06-21 11:08:00 -0300

    removed upstream

--- python_service/src/api/master.py
+++ python_service/src/api/master.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request
 from services.file_services import *
 import pybreaker
 from utils.limiter import limiter
-    
+import socket
 master_route = Blueprint('master_route', __name__)
 
 @master_route.route("/api/v1/<provider>/file", methods=['POST'])
@@ -14,7 +14,7 @@ def post_file(provider):
     except pybreaker.CircuitBreakerError:
         return f'System Unstable! Please retry again later!', 500
     
-@master_route.route("/free", methods=['get'])
+@master_route.route("/hostname", methods=['get'])
 @limiter.exempt
 def get_Free():
-    return f'Freeeee', 200
+    return f'Hosname: {socket.gethostname()}', 200
