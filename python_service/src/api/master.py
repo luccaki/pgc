19df16d96b9c90460626f899b5500d25cadbed18
luccaki <lucca.ianaguivara@gmail.com>
2023-06-27 20:47:28 -0300

    added get_service

--- python_service/src/api/master.py
+++ python_service/src/api/master.py
@@ -6,6 +6,7 @@ import socket
 master_route = Blueprint('master_route', __name__)
 
 @master_route.route("/api/v1/<provider>/file", methods=['POST'])
+@limiter.exempt
 def post_file(provider):
     try:
         file = request.files['file']
@@ -14,6 +15,15 @@ def post_file(provider):
     except pybreaker.CircuitBreakerError:
         return f'System Unstable! Please retry again later!', 500
     
+@master_route.route("/api/v1/<provider>/file/<file_name>", methods=['GET'])
+@limiter.exempt
+def get_file(provider, file_name):
+    try:
+        res = get_service(provider, file_name)
+        return res
+    except pybreaker.CircuitBreakerError:
+        return f'System Unstable! Please retry again later!', 500
+
 @master_route.route("/hostname", methods=['get'])
 @limiter.exempt
 def get_Free():
--- python_service/src/services/file_services.py
+++ python_service/src/services/file_services.py
@@ -33,5 +33,32 @@ def post_service(provider, file):
     
     files = {'file': (file.filename, file)}
     response = requests.post(url, headers=headers, files=files)
-    response.raise_for_status()
+    return Response(response, response.status_code)
+
+def get_service(provider, file_name):
+    if(provider == 's3'):
+        credentials = get_s3_credentials()
+        url = f'http://app_s3:3002/s3/v1/file/pgc-luccaki/{file_name}'
+        headers = {
+            'Access-Key': credentials['access_key'],
+            'Secret-Key': credentials['secret_key']
+        }
+    elif(provider == 'googledrive'):
+        credentials = get_google_credentials()
+        url = f'http://app_google_drive:3003/googledrive/v1/file/{file_name}'
+        headers = {
+            'Project-Id': credentials['project_id'],
+            'Private-Key-Id': credentials['private_key_id'],
+            'Private-Key': credentials['private_key'],
+            'Client-Email': credentials['client_email'],
+            'Client-Id': credentials['client_id'],
+            'Client-X509-Cert-Url': credentials['client_x509_cert_url']
+        }
+    elif(provider == 'ipfs'):
+        url = f'http://app_ipfs:3004/ipfs/v1/file/{file_name}'
+        headers = {}
+    else:
+        return Response(f'Wrong provider specified! ({provider})', 400)
+    
+    response = requests.get(url, headers=headers)
     return Response(response, response.status_code)
